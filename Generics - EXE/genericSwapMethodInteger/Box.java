191794d3d5d5071e870406db0bf87013f510710b
Mishel Milenov <43663474+mischelll@users.noreply.github.com>
2019-10-24 22:13:03 +0300

    Add files via upload

---
+++ Generics - EXE/genericSwapMethodInteger/Box.java
@@ -0,0 +1,30 @@
+package JavaAdvancedExe.genericSwapMethodInteger;
+
+import java.util.List;
+
+public class Box<T> {
+    private List<T> list;
+
+    public Box(List<T> list) {
+        this.list = list;
+    }
+
+    public void swap(List<T> list, int index1, int index2) {
+        T element1 = list.get(index1);
+        T element2 = list.get(index2);
+
+        list.set(index2, element1);
+        list.set(index1, element2);
+
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (T t : list) {
+            stringBuilder.append(t.getClass().getName() + ": " + t);
+            stringBuilder.append(System.getProperty("line.separator"));
+        }
+
+        return stringBuilder.toString();
+    }
+}
