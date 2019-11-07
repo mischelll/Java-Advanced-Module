6ffa1317dc0c3b98c5270249441a11a3280c96c9
Mishel Milenov <43663474+mischelll@users.noreply.github.com>
2019-11-07 11:36:48 +0200

    Add files via upload

---
+++ Java OOP/Inheritance - EXE/hierarchy/Main.java
@@ -0,0 +1,9 @@
+package javaoopexe.hierarchy;
+
+public class Main {
+    public static void main(String[] args) {
+        SportCar sportCar = new SportCar(15, 330);
+        sportCar.drive(20);
+        System.out.println();
+    }
+}
---
+++ Java OOP/Inheritance - EXE/hierarchy/Motorcycle.java
@@ -0,0 +1,7 @@
+package javaoopexe.hierarchy;
+
+public class Motorcycle extends Vehicle {
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
